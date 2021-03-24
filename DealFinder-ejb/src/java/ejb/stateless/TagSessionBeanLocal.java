39f1bdacacbfdb3c571d9af4fa4d351e3b992680
aarondashtan <aarontyz98@gmail.com>
2021-03-25 01:23:07 +0800

    Commit changes from pull action

---
+++ DealFinder-ejb/src/java/ejb/stateless/TagSessionBeanLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.stateless;
+
+import entity.Tag;
+import java.util.List;
+import javax.ejb.Local;
+import util.exception.CreateNewTagException;
+import util.exception.DeleteTagException;
+import util.exception.InputDataValidationException;
+import util.exception.TagNotFoundException;
+import util.exception.UpdateTagException;
+
+/**
+ *
+ * @author Aaron Tan
+ */
+@Local
+public interface TagSessionBeanLocal {
+    public Tag createNewTagEntity(Tag newTagEntity) throws InputDataValidationException, CreateNewTagException;
+    
+    public List<Tag> retrieveAllTags();
+    
+    public Tag retrieveTagByTagId(Long tagId) throws TagNotFoundException;
+    
+    public void updateTag(Tag tagEntity) throws InputDataValidationException, TagNotFoundException, UpdateTagException;
+    
+    public void deleteTag(Long tagId) throws TagNotFoundException, DeleteTagException;
+    
+    
+}
