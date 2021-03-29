6c7a1ed0756586a27b459332f75924bb506b0e45
hewyeerou <e0417583@u.nus.edu>
2021-03-29 18:18:25 +0800

    before pull

--- DealFinder-ejb/src/java/ejb/session/stateless/DealSessionBeanLocal.java
+++ DealFinder-ejb/src/java/ejb/session/stateless/DealSessionBeanLocal.java
@@ -48,4 +48,6 @@ public interface DealSessionBeanLocal {
     public void updateDeal(Deal deal, Long categoryId, List<Long> tagIds) throws DealNotFoundException, InputDataValidationException, UpdateDealException, TagNotFoundException, CategoryNotFoundException;
    
     public void deleteDeal(Long dealId) throws DeleteDealException, DealNotFoundException;
+
+    public List<Deal> retrieveDealByBusinessId(Long businessId);
 }
